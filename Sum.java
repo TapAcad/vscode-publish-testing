ac3ac1745985582c98874fcb0172c3623eb04504
TapAcad <124581045+TapAcad@users.noreply.github.com>
2023-02-06 18:13:24 +0530

    sum

---
+++ Sum.java
@@ -0,0 +1,10 @@
+!import java.util.*;
+     public class Sum {
+     public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     int a = scan.nextInt();
+     int b = scan.nextInt();
+     int sum = a + b;
+       System.out.println( sum);
+}
+}
